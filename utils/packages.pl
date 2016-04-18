bef7422aa4ef7059398380e02c49454f41790ecb
x86_64 <x86mail@gmail.com>
2016-04-18 21:12:26 +0300

    Generate versions file

---
+++ utils/packages.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl 
+
+use lib '../lib';
+use lib 'lib';
+use File::Slurp;
+use Time::ParseDate qw/parsedate/;
+use Gentoo::CPAN::Object;
+
+my @lines = read_file($ARGV[0]);
+
+my $approve;
+
+foreach my $line (@lines){
+	if($line =~ m@(authors/id/.*)\.(meta|readme)$@){
+		$approve->{$1} = 1;
+		next;
+	}
+	next unless $line =~ m@(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) ((authors/id/.*)\.(tar\.(gz|bz2|xz|Z)|zip|tgz|tbz2))$@;
+	my ($date, $package, $package_short) = (scalar parsedate($1), $2, $3);
+	
+	my $co = Gentoo::CPAN::Object->new({
+		src_uri => $package,
+	});
+	next unless $co->is_authorized;
+	next unless $approve->{$package_short};
+	
+	printf "%d %s\n", $date, $package;
+}
--- utils/versions.pl
+++ utils/versions.pl
@@ -1,77 +1,40 @@
 #!/usr/bin/perl 
 
-use File::Find;
+use lib '../lib';
 use File::Slurp;
 use Data::Dumper;
 use Gentoo;
 use Gentoo::CPAN::Object;
-use YAML::XS qw/DumpFile/;
+use YAML qw/DumpFile/;
 
-our $folder = "cpan_mirror";
-our $output = "data/versions";
+our ($input, $output) = @ARGV;
 
 our $g = Gentoo->new;
 our $db = {};
 
-find({
-	wanted => sub {
-		my $file = $_;
-		
-		next unless $file =~ /CHECKSUMS/;
-		
-		my $content = read_file($file);
-		my $checksums = eval $content;
-		next if $@;
-		
-		process_checksums($checksums);
-	},
-	no_chdir => 1,
-}, $folder);
+foreach my $line (read_file($input)){
+	chomp $line;
+	my ($mtime, $name) = split / /, $line;
 	
-foreach my $package_name (keys %$db){
-	$db->{$package_name} = [
-		sort {
-			$a->{d} <=> $b->{d} ||
-			$a->{v} cmp $b->{v}
-		}
-		@{ $db->{$package_name} }
+	my $co = Gentoo::CPAN::Object->new({
+		parent => $g,
+		src_uri => $name,
+	});
+	
+	push @{ $db->{$co->package_name} }, {
+		mtime   => $mtime,
+		version => $co->package_version,
+		src_uri => $name,
+	};
+}
+
+foreach my $package (keys %$db){
+	$db->{$package} = [
+		grep { defined }
+		map { $_->{v} }
+		sort { $a->{d} <=> $b->{d} }
+		@{ $db->{$package} }
 	];
 }
 
 DumpFile($output, $db);
-
-sub process_checksums {
-	my ($checksums) = @_;
-
-	my @items;
-	foreach my $name (keys %$checksums){
-		next unless $name =~ /^(.*)\.meta$/;
-		my $key = $1;
-				
-		my $tar_key = (
-			sort { length($a) <=> length($a) }
-			grep { index($_, "${key}.") == 0 }
-			grep { not /\.(meta|readme|CHANGELOG|asc|sig|gpg|crc|gzrawtrue)$/i }
-			keys %$checksums
-		)[0]
-			or next;
-		my $tar = $checksums->{$tar_key}
-			or next;
-		
-		push @items, { name => $tar_key, mtime => $tar->{mtime} };
-	}	
-	
-	foreach my $item (@items){
-		my $co = Gentoo::CPAN::Object->new({
-			parent => $g,
-			_cpan_info => {
-				src_uri => $item->{name},
-			},
-		});
-		
-		push @{ $db->{$co->package_name} }, {
-			d => $item->{mtime},
-			v => $co->package_version,
-		};
-	}
-}
