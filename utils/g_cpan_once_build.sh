5fb5dd58ab377a297b1d547d1619e8722192f9b6
x86_64 <x86mail@gmail.com>
2016-04-24 17:48:57 +0000

    g_cpan_once_build: using parallel

--- utils/g_cpan_once_build.sh
+++ utils/g_cpan_once_build.sh
@@ -3,7 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-cat data/packages_recent | while read p; do
-	s=`echo $p | sed "s@[/.]*@_@g"`
-	timeout 120s g-cpan-once -o .. $p 2>&1 | tee -a logs/$s.log
-done;
+cat data/packages_recent | parallel -t -j8 'mkdir -p `dirname logs/{}`; timeout 120s g-cpan-once -o ../ {} 2>&1 | tee -a logs/{}.log'
