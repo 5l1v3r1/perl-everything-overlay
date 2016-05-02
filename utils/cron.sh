5fc23f8b4a6e62139facb0563b2d9f6b3f56d5d0
x86_64 <x86mail@gmail.com>
2016-05-02 15:35:10 +0000

    Cron script fixes

--- utils/cron.sh
+++ utils/cron.sh
@@ -3,6 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+git pull
 make clean-everything all overlay && \
 	git add --all ../ &&
-	git commit -a -m "Update" --author "g_cpan_once <g-cpan@server.com>"
+	git commit -a -m "Update" --author "g_cpan_once <g-cpan@server.com>" &&
+	git push
