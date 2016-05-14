da44b54b23829814fcde3aeca9a9a54fbac5b774
x86_64 <x86mail@gmail.com>
2016-05-14 07:06:51 +0000

    Remove unused directories

--- utils/g_cpan_once_build.sh
+++ utils/g_cpan_once_build.sh
@@ -3,5 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+find ../dev-perl/ -mindepth 1 -maxdepth 1 -type d -exec touch {}/.kill \;
+
 cat data/packages_recent | \
 	parallel -t -j8 "./g_cpan_once_build_one.sh $*"
+
+find ../dev-perl/ -name '.kill' | sed "s@/.kill@@g" | parallel -j1 'rm -rf {}'
