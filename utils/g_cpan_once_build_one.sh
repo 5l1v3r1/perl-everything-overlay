52d4047b3f7677954ba8ddca839b51f65e0c098c
g_cpan_once <g-cpan@server.com>
2018-07-07 09:25:47 +0000

    Update

--- utils/g_cpan_once_build_one.sh
+++ utils/g_cpan_once_build_one.sh
@@ -6,6 +6,6 @@ cd $DIR
 cpan_tar=$1
 
 mkdir -p `dirname logs/${cpan_tar}`
-timeout 80s g-cpan-once -o ${DIR}/../ ${cpan_tar} $2 $3 $4 >logs/${cpan_tar}.log 2>&1
+timeout 600s g-cpan-once -o ${DIR}/../ ${cpan_tar} $2 $3 $4 >logs/${cpan_tar}.log 2>&1
 
 exit 0
